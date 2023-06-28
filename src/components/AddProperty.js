e550a221b42257cacf5ca1fc90a4ececbd0e2674
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-06-28 07:25:27 +0100

    Create form for Add Property component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
-  <>Add Property Page</>;
+  const initialState = {
+    fields: {
+      title: "",
+      type: "Flat",
+      bedrooms: 1,
+      bathrooms: 1,
+      price: 0,
+      city: "Manchester",
+      email: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="add-property">
+      Add Property Page
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            type="text"
+            placeholder="Family home"
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="type">
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <input
+            type="number"
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <input
+            type="number"
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffields">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            placeholder="jane.doe@email.co.uk"
+            id="email"
+            name="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProperty;
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "../styles/app.css";
 import Navbar from "./Navbar";
 import Properties from "./Properties";
