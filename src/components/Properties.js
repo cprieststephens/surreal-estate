53d210851c211078d713c5f8475caca4791671df
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-07-05 12:18:27 +0100

    Add Properties component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
+import "../styles/properties.css";
 
 const Properties = () => {
-  <>Properties</>;
+  const [properties, setProperties] = useState([]);
+
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/v1/PropertyListing`)
+      .then(({ data }) => setProperties(data))
+      .catch(() =>
+        setAlert({
+          message: "Server error. Please try again later.",
+        })
+      );
+  }, []);
+
+  return (
+    <div>
+      {properties.map((property) => (
+        <PropertyCard key={property._id} {...property} />
+      ))}
+      <div className="alert-container">
+        <Alert message={alert.message} />
+      </div>
+    </div>
+  );
 };
 
 export default Properties;
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -19,34 +19,40 @@ const propertyCard = ({
   type,
 }) => {
   return (
-    <div className="property-card">
-      <div className="property-card_logo-container">
-        <img className="property-card__logo" src={logo} alt="Surreal Estate logo" />
-      </div>
-      <div className="property-card__item">
-        <span className="property-card__bold">{title}</span>
-      </div>
-      <div className="property-card__item">
-        <span className="property-card__italic">{type} - </span>
-        <span className="property-card__italic">{city}</span>
-      </div>
-      <div className="property-card__item">
-        <FontAwesomeIcon className="property-card__icon" icon={faBath} />
-        <span> {bathrooms}</span>
-      </div>
-      <div className="property-card__item">
-        <FontAwesomeIcon className="property-card__icon" icon={faBed} />
-        <span> {bedrooms}</span>
-      </div>
-      <div className="property-card__item">
-        <FontAwesomeIcon icon={faSterlingSign} />
-        <span> {price}</span>
-      </div>
-      <div className="property-card__email">
-        <a className="property-card__link" href={`mailto:${email}`}>
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span> Email</span>
-        </a>
+    <div className="property-card__container">
+      <div className="property-card">
+        <div className="property-card_logo-container">
+          <img
+            className="property-card__logo"
+            src={logo}
+            alt="Surreal Estate logo"
+          />
+        </div>
+        <div className="property-card__item">
+          <span className="property-card__bold">{title}</span>
+        </div>
+        <div className="property-card__item">
+          <span className="property-card__italic">{type} - </span>
+          <span className="property-card__italic">{city}</span>
+        </div>
+        <div className="property-card__item">
+          <FontAwesomeIcon className="property-card__icon" icon={faBath} />
+          <span> {bathrooms}</span>
+        </div>
+        <div className="property-card__item">
+          <FontAwesomeIcon className="property-card__icon" icon={faBed} />
+          <span> {bedrooms}</span>
+        </div>
+        <div className="property-card__item">
+          <FontAwesomeIcon icon={faSterlingSign} />
+          <span> {price}</span>
+        </div>
+        <div className="property-card__email">
+          <a className="property-card__link" href={`mailto:${email}`}>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span> Email</span>
+          </a>
+        </div>
       </div>
     </div>
   );
