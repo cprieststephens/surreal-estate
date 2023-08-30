ebcdf97e163d687fb6868d989a006352e2ef8a78
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-30 14:04:17 +0100

    Add media queries for Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUserPlus,
+  faRightToBracket,
+  faPlus,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
 import logo from "../img/logo.png";
@@ -28,28 +36,53 @@ const Navbar = ({ setUserID, userID }) => {
       <img className="navbar-logo" src={logo} alt="Surreal Estate logo" />
       <h2 className="navbar-title">Surreal Estate</h2>
       {!userID ? (
-        <div className="navbar-links">
-          <Link className="navbar-links-item" to="/">
-            View Properties
-          </Link>
-          <Link className="navbar-links-item" to="sign-up">
-            Sign Up
-          </Link>
-          <Link className="navbar-links-item" to="sign-in">
-            Sign In
-          </Link>
+        <div className="navbar-container">
+          <ul className="navbar-links">
+            <li>
+              <Link className="navbar-links-item" to="/">
+                <FontAwesomeIcon className="navbar-icon" icon={faHouse} />
+                <span className="navbar-text">View Properties</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="navbar-links-item" to="sign-up">
+                <FontAwesomeIcon className="navbar-icon" icon={faUserPlus} />
+                <span className="navbar-text">Sign Up</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="navbar-links-item" to="sign-in">
+                <FontAwesomeIcon
+                  className="navbar-icon"
+                  icon={faRightToBracket}
+                />
+                <span className="navbar-text">Sign In</span>
+              </Link>
+            </li>
+          </ul>
         </div>
       ) : (
-        <div className="navbar-links">
-          <Link className="navbar-links-item" to="/">
-            View Properties
-          </Link>
-          <Link className="navbar-links-item" to="add-property">
-            Add a Property
-          </Link>
-          <Link className="navbar-links-item" to="saved-properties">
-            Saved Properties
-          </Link>
+        <div className="navbar-container">
+          <ul className="navbar-links">
+            <li>
+              <Link className="navbar-links-item" to="/">
+                <FontAwesomeIcon className="navbar-icon" icon={faHouse} />
+                <span className="navbar-text">View Properties</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="navbar-links-item" to="add-property">
+                <FontAwesomeIcon className="navbar-icon" icon={faPlus} />
+                <span className="navbar-text">Add a Property</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="navbar-links-item" to="saved-properties">
+                <FontAwesomeIcon className="navbar-icon" icon={faStar} />
+                <span className="navbar-text">Saved Properties</span>
+              </Link>
+            </li>
+          </ul>
           <button
             className="navbar-button"
             type="button"
