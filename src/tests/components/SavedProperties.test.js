93626a7ee68e93ee28e5c03b32eb325d6b419b68
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-09-12 09:40:18 +0100

    Add test and snapshot for SavedProperties component

---
+++ src/tests/components/SavedProperties.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import axios from "axios";
+import SavedProperties from "../../components/SavedProperties";
+
+describe("SavedProperties", () => {
+  it("renders correctly", async () => {
+    const mockData = {
+      properties: [
+        {
+          id: "1111",
+          propertyListing: {
+            id: "2222",
+            title: "stub-title",
+            type: "stub-type",
+            bedrooms: "stub-bedrooms",
+            bathrooms: "stub-bathrooms",
+            price: "stub-price",
+            city: "stub-city",
+            email: "stub-email",
+            __v: "stub-value",
+          },
+          __v: "stub-value",
+        },
+      ],
+    };
+    jest.spyOn(axios, "get").mockResolvedValue(mockData);
+    const { asFragment } = render(<SavedProperties />);
+
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
